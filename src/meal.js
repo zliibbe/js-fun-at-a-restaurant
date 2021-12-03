14096a49be9f500e533ea7df730283e17d7175c3
Zach Liibbe <zliibbe@gmail.com>
2021-12-03 10:59:49 -0700

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,38 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+      menuItemName = `Delicious ${name}`
+      return menuItemName;
 }
 
+function createMenuItem(name, price, type) {
+    var menuItem = {
+      name: name,
+      price: price,
+      type: type,
+    };
+    return menuItem;
+}
+
+//var ingredients = []
+
+function addIngredients(ingredient, listOfIngredients) {
+  //var ingredients = ["cheese", ingredient]
+  //anyArray = ingredients;
+  //anyArray.push(ingredient);
+
+  //addIngredients("cheese", anyArray);
+
+  return listOfIngredients.push(ingredient);
+
+  //function addIngredients(topping1, topping2) {
+  //topping2.push(topping1)
+  //} feedback to Rio & Casey why this is not topping1 & topping2
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
--- src/warm-up.js
+++ src/warm-up.js
@@ -12,19 +12,23 @@
 // var food = "Ice Cream";
 // var type = "Dessert";
 // var inStock = 10
+var food = "chips";
+var type = "snack";
+var inStock = 11
 
-
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
+inStock = inStock * 2
+console.log(food, type, inStock);
 
-
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
+inStock = inStock - 1
+console.log(inStock);
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
+var isFavorite = false;
 
 
 
@@ -32,7 +36,13 @@
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+var shouldOrderMore = true
+if (inStock < 20) {
+  shouldOrderMore = true
+} else {
+  shouldOrderMore = false
+}
+console.log(shouldOrderMore);
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,15 +50,25 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
-
+var stockCount = 5
+if (stockCount >= 1) {
+  console.log(`I'm going to eat some ${food}!`)
+} else {
+  console.log(`we need to get some more ${food}.`)
+}
 
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
+if (isFavorite === true) {
+  console.log(`My favorite food is ${food}!`)
+} else {
+  console.log(`I recently ate ${food}`)
+}
 
 
 
@@ -57,26 +77,35 @@
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
-
+var vegetables = ["carrot", "sweet potatoes", "turnip", "kale", "onion"]
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+console.log(vegetables[2]);
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
+var fruits = ["apple", "banana", "kiwi", "orange", "lemon", "lime", "clementine"];
 
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+console.log(fruits[4]);
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable called "groceryStore" and assign to it an object literal with two
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
-
-
+var groceryStore = {
+  name: "King Soopers",
+  isOpen: true,
+  address: "Unitah Ave & 19th Street",
+  parkingSpots: 80,
+  sellsLiquor: true,
+}
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+groceryStore.cityLocation = "Denver, Colorado";
+
+console.log(groceryStore.cityLocation);
