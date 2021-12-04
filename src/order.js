03a86ba3eab921d3f527b8153ddb4d6cf8fc7090
Zach Liibbe <zliibbe@gmail.com>
2021-12-04 13:40:46 -0700

    Add code to pass takeOrder tests

--- src/order.js
+++ src/order.js
@@ -1,6 +1,25 @@
 
+//var deliveryOrders = [];
+
+function takeOrder (order, deliveryOrders) {
+      //for (var i = 0; i < 3; i++) {
+      if (deliveryOrders.length <= 2) {
+        deliveryOrders.push(order);
+      }
+      // } else{
+      //   return deliveryOrders;
+      // }
+      return
+}
+
+
+
+//function addOrder (orderNumber, item, price, orderType, status) {
+      //var order =
+// }
+
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
